41e7645702247d3bb5bc9ad51988a863efe01569
Roberto Espinoza <robertoes2697@gmail.com>
2024-03-29 23:33:44 -0500

    Add next config to allow image sources

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async redirects() {
-        return [
-          {
+   async redirects() {
+      return [
+         {
             source: '/',
             destination: '/articles', // Change this to your desired path
             permanent: true, // or false if it's a temporary redirect
-          },
-        ];
-      },
+         },
+      ];
+   },
+   images: {
+      remotePatterns: [
+         {
+            protocol: 'https',
+            hostname: '**',
+            // port: '',
+            // pathname: '/**',
+         },
+      ],
+   },
 };
 
 export default nextConfig;
