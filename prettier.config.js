4f5ae525dca0a06988495151b71d586ac205f900
Roberto Espinoza <robertoes2697@gmail.com>
2024-03-29 20:36:34 -0500

    initial setup

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    arrowParens: 'always',
+    bracketSameLine: true,
+    bracketSpacing: true,
+    embeddedLanguageFormatting: 'auto',
+    endOfLine: 'lf',
+    jsxBracketSameLine: false,
+    jsxSingleQuote: true,
+    printWidth: 120,
+    semi: true,
+    singleQuote: true,
+    tabWidth: 3,
+ };
+ 
